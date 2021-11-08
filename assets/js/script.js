128cf6a55c65a546476e9513443b17edd6c68256
tiana <tianasjohnson98@gmail.com>
2021-11-07 19:11:35 -0500

    updating forecast card

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ const inputValue = document.querySelector('#City')
 const saveLocation = document.querySelector('.history')
 const storedCity = localStorage.getItem('cityName')
 
+var mainName = document.querySelector('#city-name')
+var temp = document.querySelector('#city-temp')
+var uv = document.querySelector('#city-index')
+var humidity = document.querySelector('#city-humidity')
+var wind = document.querySelector('#city-wind')
+var now = moment()
+
 //save search input to p element
 if(input) {
 
@@ -15,19 +22,30 @@ if(input) {
 
 
 button.addEventListener('click',function(){
+    
+  saveLocalStorage();
+  dailyForecast ();
+
+//Get weather for forecast
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&units=imperial&appid=9ad0721d21a4ea8d5e5ac7b0c479ae8b').then(function(response){
+response.json().then(function(data) {
+   console.log(data);
+
+   
+
+
+   }
+
+);
+
+});
+
+
+});
  
-  saveLocalStorage()
-
-//Get weather for city
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid=9ad0721d21a4ea8d5e5ac7b0c479ae8b')
-.then(response => response.json())
-  .then (data => console.log(data))
-  .catch(err => alert("Wrong city name!"));
-
-
 
     
-})
+
 
 //save city search name under search
     
@@ -35,14 +53,51 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid=
 inputValue.addEventListener('input', letter  => {
  text.textContent = letter.target.value
 
-})
+});
+
+
 //save to local storage function
-const saveLocalStorage = () => {
+saveLocalStorage = () => {
 
 localStorage.setItem('cityName', text.textContent)
 
-}
+};
 
 
 
-  
+ 
+  function displayCityInfo() {
+
+   
+
+
+    
+  };
+
+//get weather for Day
+dailyForecast = function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=9ad0721d21a4ea8d5e5ac7b0c479ae8b').then(function(response){
+        response.json().then(function(data) {
+     
+            console.log(data);
+           
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var cityHum = data['main']['humidity'];
+            var cityWind = data['wind']['speed'];
+            
+            
+
+             mainName.innerHTML =  nameValue + ('\xa0') + now.format("MMM Do YYYY") ;
+             temp.innerHTML = ('Temperature:') + ('\xa0') + tempValue + ('\xa0') + ('\u00B0') +('F');
+             humidity.innerHTML = ('Humidity:') + ('\xa0') + cityHum + ('%');
+             wind.innerHTML = ('Wind:') + ('\xa0') +   cityWind + ('\xa0') + ('MPH') ;
+             
+           
+
+
+        });
+    
+    });
+    };
+    
